282f1b8fe53ca108d1812e2cc927c61a592c53a9
Carter Konz <carter.konz@gmail.com>
2017-04-30 19:13:45 -0500

    Removed typings.

--- src/Coordinates.ts
+++
@@ -1,4 +0,0 @@
-export class Coordinates{
-  lat: string;
-  lon: string;
-}
--- src/Location.ts
+++
@@ -1,6 +0,0 @@
-import { Coordinates } from './Coordinates';
-
-export class Location{
-  zip:string;
-  coords:Coordinates[];
-}
